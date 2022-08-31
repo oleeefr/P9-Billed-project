81af529556b58885137e7f5bac3d22ceef2bbc75
Portable-dev <oleeefr@yahoo.Fr>
2022-08-31 18:04:18 +0200

    upgrade file newBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,73 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+ import { fireEvent, screen, waitFor } from "@testing-library/dom";
+ import NewBillUI from "../views/NewBillUI.js";
+ import NewBill from "../containers/NewBill.js";
+ import BillsUI from "../views/BillsUI.js";
+ import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+ import { localStorageMock } from "../__mocks__/localStorage.js";
+ import router from "../app/Router.js";
+ import store from "../__mocks__/store";
+ import mockStore from "../__mocks__/store.js";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Envelope icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const windowIcon = screen.getByTestId("icon-mail");
+      expect(windowIcon).toBeDefined()
     })
+
+
+    describe("When I upload a file", () => {
+      test("Then the file handler should show a file", () => {
+        document.body.innerHTML = NewBillUI()
+        const newBill = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage })
+        const handleChangeFile = jest.fn(() => newBill.handleChangeFile)
+        const inputFile = screen.getByTestId("file")
+        inputFile.addEventListener("change", handleChangeFile)
+        fireEvent.change(inputFile, {
+            target: {
+                files: [new File(["sample.jpeg"], "sample.jpeg", { type: "image/jpeg" })],
+            }
+        })
+        const numberOfFile = screen.getByTestId("file").files.length
+        expect(numberOfFile).toEqual(1)
+      })
+
+      test("Then the error message should be display", async () => {
+        document.body.innerHTML = NewBillUI()
+        const newBill = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage })
+        const handleChangeFile = jest.fn(() => newBill.handleChangeFile)
+        const handleSubmit = jest.fn(()=> newBill.handleSubmit)
+        const inputFile = screen.getByTestId("file")
+        inputFile.addEventListener("change", handleChangeFile)
+        fireEvent.change(inputFile, {
+            target: {
+                files: [new File(["sample.txt"], "sample.txt", { type: "text/txt" })],
+            }
+        })
+        expect(handleChangeFile).toBeCalled()
+        expect(handleSubmit).not.toHaveBeenCalled()
+      })
+
+    })
+  
   })
 })
